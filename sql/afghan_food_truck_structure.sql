db6d3a6b3a1de29a85c2cf34e53b1079148ec472
urzraz2644 <urzraz2644@students.ecpi.edu>
2025-11-09 18:43:38 -0500

    Added Afghan Food Truck SQL exports (data, structure, full) to GitHub

---
+++ sql/afghan_food_truck_structure.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 02, 2025 at 08:08 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `afghan_food_truck`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `price` decimal(5,2) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `item_name`, `price`, `description`) VALUES
+(1, 'Chicken Kabob', '12.99', 'Grilled chicken with side of veggies and rice.'),
+(2, 'Mantu', '10.99', 'Afghan meat dumplings'),
+(3, 'Bolani', '8.99', 'Pan-fried flatbread with potato filling'),
+(4, 'Beef kabob ', '12.99', 'Grilled beef with side of veggies and rice'),
+(5, 'Ashak', '8.99', 'Veggie afghan dumpling'),
+(6, 'Veggie kabob', '10.99', 'Grilled season veggies with side of rice ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
